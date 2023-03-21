250a4542a8a4e1435b5f730710f3903fc4795b55
Alex Britto <alexbritto0509@gmail.com>
2023-03-20 23:20:58 -0300

    adjust faq screen and footer infos (#1)
    Co-authored-by: Alex Britto <alex.britto0509@gmail.com>

--- pages/faq.js
+++ pages/faq.js
@@ -1,36 +1,24 @@
-import Head from "next/head"
-import Link from "next/link"
+import FAQScreen from "../src/screens/FAQScreen"
 
-export async function getServerSideProps() {
+export default FAQScreen
+
+export async function getStaticProps() {
+  console.log("Em modo DEV, sempre roda! A cada acesso")
+  console.log("Roda SOMENTE em build time")
   const FAQ_API_URL =
     "https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json"
-
   const faq = await fetch(FAQ_API_URL)
-    .then((response) => response.json())
-    .then((response) => response)
-  return {
-    props: { faq }, // will be passed to the page component as props
-  }
-}
+    .then((respostaDoServidor) => {
+      return respostaDoServidor.json()
+    })
+    .then((resposta) => {
+      return resposta
+    })
 
-export default function FAQPage({ faq }) {
-  return (
-    <div>
-      <Head>
-        <title>FAQ - Alura Cases Campanha</title>
-      </Head>
-      <h1>Alura Cases - FAQ</h1>
-      <Link href="/">Ir para a Home</Link>
-      <ul>
-        {faq.map(({ answer, question }) => {
-          return (
-            <article key={question}>
-              <h2>{question}</h2>
-              <p>{answer}</p>
-            </article>
-          )
-        })}
-      </ul>
-    </div>
-  )
+  return {
+    props: {
+      qualquercoisa: "que eu passar aqui",
+      faq,
+    },
+  }
 }
