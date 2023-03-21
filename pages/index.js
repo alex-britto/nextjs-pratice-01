c5ed92f5aed7fb0299aa7083b0bcc74cbabb1a1c
Alex Britto <alex.britto0509@gmail.com>
2023-03-20 22:50:40 -0300

    initial commit

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import HomeScreen from "../src/screens/HomeScreen"
+
+export default HomeScreen
+
+// import Link from "next/link"
+// import styled from "styled-components"
+
+// export default function HomePage() {
+//   return (
+//     <div>
+//       <Title as="h2">Alura Cases - Home Page</Title>
+//       <Link href="/faq">Ir para o FAQ</Link>
+//     </div>
+//   )
+// }
+
+// const Title = styled.h1`
+//   color: red;
+// `
